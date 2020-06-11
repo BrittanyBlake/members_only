d91f80de8f87cbcddf979a1d3c30664b9c7b6c19
Brittany Blake <brittany.blake49@gmail.com>
2020-06-11 16:26:36 +0200

    create migration table for posts

--- db/migrate/20200611135854_create_posts.rb
+++ db/migrate/20200611135854_create_posts.rb
@@ -1,6 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.string :title
+      t.string :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_135016) do
+ActiveRecord::Schema.define(version: 2020_06_11_135854) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
